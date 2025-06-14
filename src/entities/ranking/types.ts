de481b84bd2bcbff4fa3fa8311939c693a679d60
Hrybok Alex <zagadum@ukr.net>
2025-06-14 16:34:32 +0300

    fix

--- src/entities/ranking/types.ts
+++ src/entities/ranking/types.ts
@@ -4,10 +4,13 @@
 //   points: number;
 // }
 
-export interface Ranking {
-  place: number;
-  name: string;
-  level: string
-  age: string;
-  points: number;
-}
+ export interface Ranking {
+    place: number;
+    surname: string;
+    lastname: string;
+    stages_level: string;
+    age_tab: string;
+    good_answear: number;
+    points: number;
+  }
+
