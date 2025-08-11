094e321113d6931dc666704279196904b422965c
alekseyflorya <aleksey.florya@gmail.com>
2025-08-11 22:19:29 +0300

    fix: update nav links

--- src/shared/config/index.ts
+++ src/shared/config/index.ts
@@ -1,3 +1,6 @@
 export const API_URL = import.meta.env.DEV
   ? undefined
   : "https://api-memory.firm.kiev.ua";
+
+export const UK_URL = import.meta.env.DEV ? 'https://memory.firm.kiev.ua' : 'https://space-memory.com'
+export const PL_URL = import.meta.env.DEV ? 'https://memory-pl.firm.kiev.ua' : 'https://pl.space-memory.com'
--- src/shared/lib/getNavItems.ts
+++ src/shared/lib/getNavItems.ts
@@ -3,28 +3,41 @@ import homeworkIcon from "@/shared/assets/icons/homework.svg";
 import trainingIcon from "@/shared/assets/icons/training.svg";
 import olympiadIcon from "@/shared/assets/icons/olympiad.svg";
 import { TFunction } from "i18next";
+import { PL_URL, UK_URL } from "@/shared/config";
 
 export const getNavItems = (t: TFunction<"translation", undefined>) => [
   {
     icon: homeIcon,
     label: t("sidebar.home"),
-    link: "https://memory.firm.kiev.ua/student",
+    link: {
+      uk: `${UK_URL}/student`,
+      pl: `${PL_URL}/student`,
+    },
   },
   {
     icon: homeworkIcon,
     label: t("sidebar.homework"),
     // link: "/homework",
-    link: "https://memory.firm.kiev.ua/student/hometask",
+    link: {
+      uk: `${UK_URL}/student/hometask`,
+      pl: `${PL_URL}/student/hometask`,
+    },
   },
   {
     icon: trainingIcon,
     label: t("sidebar.training"),
     // link: "/training",
-    link: "https://memory.firm.kiev.ua/student/traning/create",
+    link: {
+      uk: `${UK_URL}/student/traning/create`,
+      pl: `${PL_URL}/student/traning/create`,
+    },
   },
   {
     icon: olympiadIcon,
     label: t("sidebar.olympiads"),
-    link: "/olympiads",
+    link: {
+      uk: "/olympiads",
+      pl: "/olympiads",
+    },
   },
 ];
