174c5e2c21b016b1ab16936bbd07ff37007b1b7e
alekseyflorya <aleksey.florya@gmail.com>
2025-08-11 22:34:17 +0300

    update config

--- src/shared/config/index.ts
+++ src/shared/config/index.ts
@@ -2,5 +2,10 @@ export const API_URL = import.meta.env.DEV
   ? undefined
   : "https://api-memory.firm.kiev.ua";
 
-export const UK_URL = import.meta.env.DEV ? 'https://memory.firm.kiev.ua' : 'https://space-memory.com'
-export const PL_URL = import.meta.env.DEV ? 'https://memory-pl.firm.kiev.ua' : 'https://pl.space-memory.com'
+//PROD
+// export const UK_URL = import.meta.env.DEV ? 'https://memory.firm.kiev.ua' : 'https://space-memory.com'
+// export const PL_URL = import.meta.env.DEV ? 'https://memory-pl.firm.kiev.ua' : 'https://pl.space-memory.com'
+
+//STAGE
+export const UK_URL = 'https://memory.firm.kiev.ua'
+export const PL_URL = 'https://memory-pl.firm.kiev.ua'
