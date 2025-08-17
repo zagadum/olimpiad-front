47c8be512cca0f86f1b96010bbb5dd49064c3a21
Hrybok Alex <zagadum@ukr.net>
2025-08-17 14:36:53 +0300

    add swithc lang ?lang=uk

--- src/pages/auth/auth-token.tsx
+++ src/pages/auth/auth-token.tsx
@@ -1,18 +1,24 @@
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import { useTranslation } from "react-i18next";
 
 const AuthPage = () => {
   const navigate = useNavigate();
-
+  const { i18n } = useTranslation();
   useEffect(() => {
     const params = new URLSearchParams(window.location.search);
     const token = params.get('token');
 
+    const lang = params.get("lang");
+    if (lang) {
+      i18n.changeLanguage(lang);
+    }
+    document.documentElement.lang = i18n.language;
     if (token) {
       localStorage.setItem('token', token);
     }
     navigate('/olympiads/all');
-  }, [navigate]);
+  }, [navigate,i18n.language]);
 
   return <div>Проверка авторизации...</div>;
 };
