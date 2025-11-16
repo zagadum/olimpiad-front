53d1c361338f6bfaf14b7254cbcfedfef20e1d9d
Hrybok Alex <zagadum@ukr.net>
2025-11-16 02:37:48 +0200

    add m_payments test fix

--- src/entities/payments/api.ts
+++ src/entities/payments/api.ts
@@ -1,6 +1,6 @@
 // src/entities/payments/api.ts
 import { axiosInstance } from "@/shared/api/axiosInstance";
-import i18n from "@/shared/i18n";
+
 import {
   PaymentCreateRequest,
   PaymentCreateResponse,
@@ -9,10 +9,6 @@ import {
 export const createPayment = async (
   data: PaymentCreateRequest,
 ): Promise<PaymentCreateResponse> => {
-  const res = await axiosInstance.post("/api/payment/create", data, {
-    headers: {
-      "Accept-Language": data.lang || i18n.language,
-    },
-  });
+  const res = await axiosInstance.post("/api/payment/create", data);
   return res.data;
 };
