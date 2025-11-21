4b08bebc94820b17cf7274611eaf52b89765991f
Farid <fgaydarov7@gmail.com>
2025-11-21 20:59:55 +0100

    F. Design Css for payment_fail page

--- src/pages/olympiad/payment-status/fail.tsx
+++ src/pages/olympiad/payment-status/fail.tsx
@@ -12,30 +12,30 @@ export const PaymentFailPage: React.FC = () => {
   const description = useLocalizedDescription(failDescription);
 
   return (
-    <div className="flex w-full flex-col items-center gap-10">
+    <div className="flex w-full flex-col items-center gap-2">
       <div
         className={cn(
-          "mt-10 w-full rounded-xl bg-gradient-to-t from-[#340808] to-[#4D1919] px-4 pb-12 pt-8",
-          "md:mt-20 md:max-w-[1286px] md:rounded-3xl md:px-16 md:pb-24 md:pt-16"
+          "mt-10 w-full rounded-xl bg-[#193C4D80] px-4 pb-12 pt-8",
+          "md:mt-20 md:max-w-[1286px] md:rounded-3xl md:px-6 md:pb-14 md:pt-10"
         )}
       >
-        <h2 className="mb-6 text-xl font-bold md:px-4 md:mb-10 md:text-3xl">
+        <h2 className="mb-6 text-xl font-bold md:px-4 md:mb-10 md:text-1xl">
           {t("paymentStatus.error.title")}
         </h2>
         <div
-          className="rounded-xl bg-[--color-5] p-4 text-sm text-[--color-placeholder] md:rounded-3xl md:text-base"
+          className="rounded-xl bg-[--color-5] pt-8 pb-8 p-4 text-sm text-[--color-placeholder] md:rounded-3xl md:text-base  min-h-[50vh]"
           dangerouslySetInnerHTML={{ __html: description }}
         />
       </div>
       <div className="w-full max-w-[1286px] flex gap-4 md:p-4">
         <Button
-          className="flex-1 text-base lg:px-8 lg:py-3"
+          className="w-[30%] text-base lg:px-8 lg:py-3"
           onClick={() => navigate("..")}
         >
           {t("common.back")}
         </Button>
         <Button
-          className="flex-1 text-base lg:px-8 lg:py-3"
+          className="w-[30%] text-base lg:px-8 lg:py-3"
           onClick={() => window.location.reload()}
         >
           {t("paymentStatus.retry")}
