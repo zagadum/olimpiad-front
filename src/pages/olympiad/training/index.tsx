db30cc42f56362175b76606eba9cdd94c97456e5
Hrybok Alex <zagadum@ukr.net>
2026-02-11 14:31:49 +0200

    https://trello.com/c/FXdNzWM1/279 show balls

--- src/pages/olympiad/training/index.tsx
+++ src/pages/olympiad/training/index.tsx
@@ -309,7 +309,10 @@ export const TrainingPage: React.FC = () => {
                           ? t("olympiadStart.done")
                           : t("olympiadTraining.startTraining")}
                       </Button>
-                        {item.points != null ? `${item.points} ${t("olympiadStart.points")}` : ''}
+                        <p className="max-w-[395px] text-xs font-light text-[#A5A5A5] lg:text-base">
+                            {item.points != null ? `${item.points} ${t("olympiadStart.points")}` : ''}
+                        </p>
+
                     </td>
                   </tr>
                 );
